40dbabf6b7838b9bd2c83f792b656ec7f7ffcef6
ch <mkmsacc@outlook.com>
2020-11-12 19:26:59 +0700

    Add tests

--- src/test/java/com/example/actuatorservice/ActuatorserviceApplicationTests.java
+++ src/test/java/com/example/actuatorservice/ActuatorserviceApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.actuatorservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = { "management.port=0" })
 class ActuatorserviceApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Value("${local.management.port}")
+	private int mgt;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	public void shouldReturn200WhenSendingRequestToController()
+			throws Exception {
+
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+				"http://localhost:" + this.port + "/hello-world", Map.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
+	@Test
+	public void shouldReturn200WhenSendingRequestToManagementEndpoint()
+			throws Exception {
+
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+				"http://localhost:" + this.mgt + "/actuator/info", Map.class);
+
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
